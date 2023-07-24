c9b4ba381964a681308e974a18f932707e23eec1
andres <andrecitoramirez11@gmail.com>
2023-07-24 16:02:14 -0500

    prueba2

--- KATAS/kata.py
+++ KATAS/kata.py
@@ -1,5 +1,5 @@
 #ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
-#If the function is passed a valid PIN string, return true, else return f alse
+#If the function is passed a valid PIN string, return true, else return false
 def lectura(string,c=0):
     numeros = "1234567890"
     try:
