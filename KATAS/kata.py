caafbe5342b03ce78076697668d386c94cab246f
andres <andrecitoramirez11@gmail.com>
2023-07-24 16:00:50 -0500

    kata7

---
+++ KATAS/kata.py
@@ -0,0 +1,15 @@
+#ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+#If the function is passed a valid PIN string, return true, else return false
+def lectura(string,c=0):
+    numeros = "1234567890"
+    try:
+        if string[c] in numeros:
+            return lectura(string,c+1)
+        else:
+            return False
+    except IndexError as error:
+        return True
+        
+def validate_pin(pin):
+    if len(pin)==4 or len(pin)==6:
+        return lectura(pin)
